7d573bd839016e4736c34956b37367e583484cb8
HeyTon888 <thayyakrcumpa@gmail.com>
2026-08-27 14:14:10 +0700

    print
    print

--- bin/mini_project.dart
+++ bin/mini_project.dart
@@ -2,4 +2,17 @@ import 'package:mini_project/mini_project.dart' as mini_project;
 
 void main(List<String> arguments) {
   print('Hello world: ${mini_project.calculate()}!');
+  void show() {
+  print('--- Expense Tracker ---');
+  print('');
+  print('--- Main Menu ---   ');
+  print('1. Show expenses');
+  print('2. Add expense');
+  print('3. View expenses');
+  print('4. Edit expense');
+  print('5. Delete expense');
+  print('6.');
 }
+}
+
+
